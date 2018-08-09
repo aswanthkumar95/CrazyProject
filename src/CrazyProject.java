0dbed55bca1cc636502e62c47c7bfc1007546490
alatchipatruni <aswanth13cs300192@gmail.com>
2018-08-09 15:11:37 +0530

    added crazy message

--- src/CrazyProject.java
+++ src/CrazyProject.java
@@ -1,6 +1,11 @@
 public class CrazyProject {
 
     public static void main(String[] args){
+
         System.out.println("This is a crazy program");
+
+        System.out.println("It says it is crazy, but nothing crazy about it yet");
+
+        
     }
 }
